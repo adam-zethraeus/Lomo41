88480c381d81cdb9b839f1a7a8f843949216c3d8
zethraeus <adam@zethrae.us>
2013-12-11 01:09:02 -0800

    pulled in some superficial stuff from the AVCam demo.

--- Lomo41/LoViewController.m
+++ Lomo41/LoViewController.m
@@ -7,23 +7,66 @@
 //
 
 #import "LoViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface LoViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *shootView;
+@property (weak, nonatomic) IBOutlet UIButton *shootButton;
+@property (nonatomic) dispatch_queue_t sessionQueue;
+@property (nonatomic) AVCaptureSession *session;
+@property (nonatomic) AVCaptureDeviceInput *videoDeviceInput;
+@property (nonatomic) AVCaptureStillImageOutput *stillImageOutput;
+@property (nonatomic) BOOL hasCameraPermission;
+- (IBAction)doShoot:(id)sender;
 @end
 
 @implementation LoViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.session = [[AVCaptureSession alloc] init];
+	[self checkCameraPermissions];
+	self.sessionQueue = dispatch_queue_create("session queue", DISPATCH_QUEUE_SERIAL);
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)prefersStatusBarHidden {
+	return YES;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+}
+
+- (IBAction)doShoot:(id)sender {
+    NSLog(@"This is where the action happens.");
+    [self runCaptureAnimation];
+}
+
+- (void)runCaptureAnimation {
+	dispatch_async(dispatch_get_main_queue(), ^{
+		[self.shootView.layer setOpacity:0.0];
+		[UIView animateWithDuration:.25 animations:^{
+            [self.shootView.layer setOpacity:1.0];
+		}];
+	});
+}
+
+- (void)checkCameraPermissions {
+	NSString *mediaType = AVMediaTypeVideo;
+	[AVCaptureDevice requestAccessForMediaType:mediaType completionHandler:^(BOOL granted) {
+		self.hasCameraPermission = granted;
+		if (!granted) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+				[[[UIAlertView alloc] initWithTitle:@"Permission Issue"
+											message:@"Lomo41 needs permission to access your camera. Please grant it in settings."
+										   delegate:self
+								  cancelButtonTitle:@"OK"
+								  otherButtonTitles:nil] show];
+			});
+        }
+	}];
+}
 @end
