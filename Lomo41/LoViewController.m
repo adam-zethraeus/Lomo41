42f814021ae9fd2ac7063713cc891ec5fb3eb716
zethraeus <adam@zethrae.us>
2013-12-14 20:31:31 -0800

    logic to setup, and take and save photo imported from AVCam demo

--- Lomo41/LoViewController.m
+++ Lomo41/LoViewController.m
@@ -7,7 +7,11 @@
 //
 
 #import "LoViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 #import <AVFoundation/AVFoundation.h>
+
+static void * CapturingStillImageContext = &CapturingStillImageContext;
+static void * SessionRunningCameraPermissionContext = &SessionRunningCameraPermissionContext;
 
 @interface LoViewController ()
 @property (weak, nonatomic) IBOutlet UIView *shootView;
@@ -16,17 +20,85 @@
 @property (nonatomic) AVCaptureSession *session;
 @property (nonatomic) AVCaptureDeviceInput *videoDeviceInput;
 @property (nonatomic) AVCaptureStillImageOutput *stillImageOutput;
+@property (nonatomic) id runtimeErrorHandlingObserver;
+@property (nonatomic, readonly, getter = isSessionRunningAndHasCameraPermission) BOOL sessionRunningAndHasCameraPermission;
 @property (nonatomic) BOOL hasCameraPermission;
 - (IBAction)doShoot:(id)sender;
 @end
 
 @implementation LoViewController
 
++ (AVCaptureDevice *)deviceWithMediaType:(NSString *)mediaType
+                      preferringPosition:(AVCaptureDevicePosition)position {
+	NSArray *devices = [AVCaptureDevice devicesWithMediaType:mediaType];
+	AVCaptureDevice *captureDevice = [devices firstObject];
+	for (AVCaptureDevice *device in devices) {
+		if ([device position] == position) {
+			captureDevice = device;
+			break;
+		}
+	}
+	return captureDevice;
+}
+
++ (NSSet *)keyPathsForValuesAffectingSessionRunningAndDeviceAuthorized {
+	return [NSSet setWithObjects:@"session.running", @"hasCameraPermission", nil];
+}
+
+- (BOOL)isSessionRunningAndHasCameraPermission {
+	return [[self session] isRunning] && [self hasCameraPermission];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.session = [[AVCaptureSession alloc] init];
 	[self checkCameraPermissions];
 	self.sessionQueue = dispatch_queue_create("session queue", DISPATCH_QUEUE_SERIAL);
+	dispatch_async(self.sessionQueue, ^{
+		//[self setBackgroundRecordingID:UIBackgroundTaskInvalid];
+		NSError *error = nil;
+		AVCaptureDevice *videoDevice = [LoViewController deviceWithMediaType:AVMediaTypeVideo preferringPosition:AVCaptureDevicePositionBack];
+		AVCaptureDeviceInput *videoDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:videoDevice error:&error];
+		if (error) {
+			NSLog(@"%@", error);
+		}
+		if ([self.session canAddInput:videoDeviceInput]) {
+			[self.session addInput:videoDeviceInput];
+			self.videoDeviceInput = videoDeviceInput;
+		}
+		AVCaptureStillImageOutput *stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+		if ([self.session canAddOutput:stillImageOutput]) {
+			[stillImageOutput setOutputSettings:@{AVVideoCodecKey:AVVideoCodecJPEG}];
+			[self.session addOutput:stillImageOutput];
+			self.stillImageOutput = stillImageOutput;
+		}
+    });
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+	dispatch_async(self.sessionQueue, ^{
+		[self addObserver:self forKeyPath:@"sessionRunningAndHasCameraPermission" options:(NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew) context:SessionRunningCameraPermissionContext];
+		[self addObserver:self forKeyPath:@"stillImageOutput.capturingStillImage" options:(NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew) context:CapturingStillImageContext];
+		__weak LoViewController *weakSelf = self;
+		self.runtimeErrorHandlingObserver = [[NSNotificationCenter defaultCenter] addObserverForName:AVCaptureSessionRuntimeErrorNotification object:[self session] queue:nil usingBlock:^(NSNotification *note) {
+			LoViewController *strongSelf = weakSelf;
+			dispatch_async(strongSelf.sessionQueue, ^{
+				// Manually restarting the session since it must have been stopped due to an error.
+				[strongSelf.session startRunning];
+			});
+		}];
+		[self.session startRunning];
+	});
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+	dispatch_async(self.sessionQueue, ^{
+		[self.session stopRunning];
+		[[NSNotificationCenter defaultCenter] removeObserver:self name:AVCaptureDeviceSubjectAreaDidChangeNotification object:[[self videoDeviceInput] device]];
+		[[NSNotificationCenter defaultCenter] removeObserver:self.runtimeErrorHandlingObserver];
+		[self removeObserver:self forKeyPath:@"sessionRunningAndHasCameraPermission" context:SessionRunningCameraPermissionContext];
+		[self removeObserver:self forKeyPath:@"stillImageOutput.capturingStillImage" context:CapturingStillImageContext];
+	});
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,17 +109,27 @@
 	return YES;
 }
 
-- (BOOL)hasRunningSessionAndCameraPermission
-{
+- (BOOL)hasRunningSessionAndCameraPermission {
 	return self.session.isRunning && self.hasCameraPermission;
 }
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context {
 }
 
 - (IBAction)doShoot:(id)sender {
-    NSLog(@"This is where the action happens.");
     [self runCaptureAnimation];
+	dispatch_async([self sessionQueue], ^{
+		[self.stillImageOutput captureStillImageAsynchronouslyFromConnection:[self.stillImageOutput connectionWithMediaType:AVMediaTypeVideo] completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+			if (imageDataSampleBuffer) {
+				NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+				UIImage *image = [[UIImage alloc] initWithData:imageData];
+				[[[ALAssetsLibrary alloc] init] writeImageToSavedPhotosAlbum:[image CGImage] orientation:(ALAssetOrientation)[image imageOrientation] completionBlock:nil];
+			}
+		}];
+	});
 }
 
 - (void)runCaptureAnimation {
