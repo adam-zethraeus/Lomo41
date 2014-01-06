fe868084ba7730cd0108fb82a666d8edf8db96ca
zethraeus <adam@zethrae.us>
2014-01-06 12:12:20 -0800

    xib for ImagePreviewCell

---
+++ LoImagePreviewCell.h
@@ -0,0 +1,15 @@
+//
+//  LoImagePreviewCell.h
+//  Lomo41
+//
+//  Created by Adam Zethraeus on 1/6/14.
+//  Copyright (c) 2014 Very Nice Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoImagePreviewCell : UICollectionViewCell
+@property (strong, nonatomic) IBOutlet UIView *frontView;
+@property (strong, nonatomic) IBOutlet UIView *backView;
+
+@end
---
+++ LoImagePreviewCell.m
@@ -0,0 +1,31 @@
+//
+//  LoImagePreviewCell.m
+//  Lomo41
+//
+//  Created by Adam Zethraeus on 1/6/14.
+//  Copyright (c) 2014 Very Nice Co. All rights reserved.
+//
+
+#import "LoImagePreviewCell.h"
+
+@implementation LoImagePreviewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
