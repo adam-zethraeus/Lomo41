2307638b5be92764dc98e4910b98d38873cc03ab
zethraeus <adam@zethrae.us>
2014-02-26 23:20:08 -0800

    copyright

--- Lomo41/ALAssetsLibrary+PhotoAlbumFunctionality.h
+++ Lomo41/ALAssetsLibrary+PhotoAlbumFunctionality.h
@@ -1,3 +1,11 @@
+//
+//  LoShotProcessor.m
+//  Lomo41
+//
+//  Created by Adam Zethraeus on 12/20/13.
+//  Copyright (c) 2013 Very Nice Co. All rights reserved.
+//
+
 #import <AssetsLibrary/AssetsLibrary.h>
 
 @interface ALAssetsLibrary(PhotoAlbumFunctionality)
--- Lomo41/ALAssetsLibrary+PhotoAlbumFunctionality.m
+++ Lomo41/ALAssetsLibrary+PhotoAlbumFunctionality.m
@@ -1,3 +1,11 @@
+//
+//  LoShotProcessor.m
+//  Lomo41
+//
+//  Created by Adam Zethraeus on 12/20/13.
+//  Copyright (c) 2013 Very Nice Co. All rights reserved.
+//
+
 #import "ALAssetsLibrary+PhotoAlbumFunctionality.h"
 
 @implementation ALAssetsLibrary(PhotoAlbumFunctionality)
