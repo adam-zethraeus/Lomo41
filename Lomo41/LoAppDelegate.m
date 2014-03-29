2d20ef9acfd37ff8294a6f2dfec3d8336fbfb4b5
zethraeus <adam@zethrae.us>
2014-03-29 23:09:54 +0000

    set tint color to be correct

--- Lomo41/LoAppDelegate.m
+++ Lomo41/LoAppDelegate.m
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.album = [[LoAlbumProxy alloc] initForAlbum:@"Lomo41"];
-    self.window.tintColor = [[UIColor alloc] initWithRed:1.0 green:0.0 blue:0.40234 alpha:1.0];
+    self.window.tintColor = [[UIColor alloc] initWithRed:0.894117647 green:0.0 blue:0.368627451 alpha:1.0];
     return YES;
 }
 							
